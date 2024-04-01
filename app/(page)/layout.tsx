b51d51cdfd7b6360bbfdb7a9c76a9d13e886cd4d
Tuan Kiet Tran <charlieismylife2003@gmail.com>
2024-04-01 16:38:07 +0700

    Init route and sidebar

---
+++ app/(page)/layout.tsx
@@ -0,0 +1,13 @@
+import { Sidebar } from "@/app/_components"
+import React, { PropsWithChildren } from "react"
+
+const layout = ({ children }: PropsWithChildren) => {
+    return (
+        <div className='flex'>
+            <Sidebar />
+            <div className='max-h-screen min-h-screen flex-1'>{children}</div>
+        </div>
+    )
+}
+
+export default layout
